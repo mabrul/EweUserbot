0042bb8fc39538de65da7f0ec883466ab1e24345
iamuput <yogaaasp73@gmail.com>
2024-02-28 14:42:31 +0700

    [•]

--- Uputt/modules/eval.py
+++ Uputt/modules/eval.py
@@ -102,7 +102,7 @@ async def _(event):
                 event.chat_id,
                 out_file,
                 force_document=True,
-                thumb="AyiinXd/resources/logo.jpg",
+                thumb="Uputt/resources/logo.jpg",
                 allow_cache=False,
                 caption=f"`{cmd}`" if len(cmd) < 998 else None,
                 reply_to=reply_to_id,
