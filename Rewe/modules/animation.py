3430cfc3b67b4ab6421f732b48d03e4237ec188f
Rewe <140919868+mabrul@users.noreply.github.com>
2024-03-29 02:52:35 +0700

    Create animation.py

---
+++ Rewe/modules/animation.py
@@ -0,0 +1,638 @@
+import asyncio
+from time import sleep
+
+from Rewe import CMD_HELP
+from Rewe.ewe import edit_or_reply, rewe_cmd
+
+from . import cmd
+
+
+@rewe_cmd(pattern="bulan$")
+async def _(event):
+    event = await edit_or_reply(event, "bulan.")
+    animation_interval = 0.2
+    animation_ttl = range(96)
+    await event.edit("bulan..")
+    animation_chars = [
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 32])
+
+
+@rewe_cmd(pattern="sayang$")
+async def _(event):
+    e = await edit_or_reply(event, "I LOVEE YOUUU 💕")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💘💞💗💕")
+    await e.edit("💘💞💕💗")
+    await e.edit("SAYANG KAMU 💝💖💘")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💘💞💕💗")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("SELAMANYA 💕")
+    await e.edit("💘💘💘💘")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("I LOVE YOUUUU")
+    await e.edit("MY BABY")
+    await e.edit("💕💞💘💝")
+    await e.edit("💘💕💞💝")
+    await e.edit("SAYANG KAMU💞")
+
+
+@rewe_cmd(pattern=r"dino(?: |$)(.*)")
+async def _(event):
+    typew = await edit_or_reply(event, "`DIN DINNN.....`")
+    sleep(1)
+    await typew.edit("`DINOOOOSAURUSSSSS!!`")
+    sleep(1)
+    await typew.edit("`🏃                        🦖`")
+    await typew.edit("`🏃                       🦖`")
+    await typew.edit("`🏃                      🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃   `LARII`          🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃WOARGH!   🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃  Huh-Huh           🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃          🦖`")
+    await typew.edit("`🏃         🦖`")
+    await typew.edit("`DIA SEMAKIN MENDEKAT!!!`")
+    sleep(1)
+    await typew.edit("`🏃       🦖`")
+    await typew.edit("`🏃      🦖`")
+    await typew.edit("`🏃     🦖`")
+    await typew.edit("`🏃    🦖`")
+    await typew.edit("`Dahlah Pasrah Aja`")
+    sleep(1)
+    await typew.edit("`🧎🦖`")
+    sleep(2)
+    await typew.edit("`-TAMAT-`")
+
+
+@rewe_cmd(pattern="gabut$")
+async def _(event):
+    e = await edit_or_reply(event, "`PERNAAHHHHH KAHHH KAUUU MENGIRA`")
+    await e.edit("`SEPEEERTIIIII APAAAA BENTUKKKKKKK CINTAAAA`")
+    await e.edit("`RAMBUUUT WARNAAA WARNII`")
+    await e.edit("`BAGAI GULALI`")
+    await e.edit("`IMUUUTTTTT LUCUUU`")
+    await e.edit("`WALAAUUUU TAK TERLALU TINGGI`")
+    await e.edit("`GW GABUUTTTT`")
+    await e.edit("`EMMMM BACOTNYA`")
+    await e.edit("`GABUTTTT WOI GABUT`")
+    await e.edit("🙈🙈🙈🙈")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("🙈🙈🙈🙈")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("`CILUUUKKK BAAAAA`")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🚶                                 🐢")
+    await e.edit("`AHHH MANTAP`")
+    await e.edit("🙉")
+    await e.edit("🙈")
+    await e.edit("🙉")
+    await e.edit("🙈")
+    await e.edit("🙉")
+    await e.edit("😂")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("`GABUT`")
+
+
+@rewe_cmd(pattern=r"terkadang(?: |$)(.*)")
+async def _(event):
+    typew = await edit_or_reply(event, "`Terkadang`")
+    sleep(1)
+    await typew.edit("`Mencintai Seseorang`")
+    sleep(1)
+    await typew.edit("`Hanya Akan Membuang Waktumu`")
+    sleep(1)
+    await typew.edit("`Ketika Waktumu Habis`")
+    sleep(1)
+    await typew.edit("`Tambah Aja 5000`")
+    sleep(1)
+    await typew.edit("`Bercanda`")
+
+
+# Create by myself @localheart
+
+
+@rewe_cmd(pattern=r"mf$")
+async def _(event):
+    await edit_or_reply(event, "`mf g dl` **ミ(ノ;_ _)ノ=3** ")
+
+
+@rewe_cmd(pattern=r"(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    if input_str == "cinta":
+        await event.edit(input_str)
+        animation_chars = [
+            "`Connecting Ke Server Cinta`",
+            "`Mencari Target Cinta`",
+            "`Mengirim Cintaku.. 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 84%\n█████████████████████▒▒▒▒ `",
+            "`Mengirim Cintaku.. 100%\n█████████CINTAKU███████████ `",
+            "`Cintaku Sekarang Sepenuhnya Terkirim Padamu, Dan Sekarang Aku Sangat Mencintai Mu, I Love You 💞`",
+        ]
+        animation_interval = 2
+        animation_ttl = range(11)
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 11])
+
+
+@rewe_cmd(pattern=r"gombal(?: |$)(.*)")
+async def _(event):
+    typew = edit_or_reply(event, "`Hai, I LOVE YOU 💞`")
+    sleep(1)
+    await typew.edit("`I LOVE YOU SO MUCH!`")
+    sleep(1)
+    await typew.edit("`I NEED YOU!`")
+    sleep(1)
+    await typew.edit("`I WANT TO BE YOUR BOYFRIEND!`")
+    sleep(1)
+    await typew.edit("`I LOVEE YOUUUU💕💗`")
+    sleep(1)
+    await typew.edit("`I LOVEE YOUUUU💗💞`")
+    sleep(1)
+    await typew.edit("`I LOVEE YOUUUU💝💗`")
+    sleep(1)
+    await typew.edit("`I LOVEE YOUUUU💟💖`")
+    sleep(1)
+    await typew.edit("`I LOVEE YOUUUU💘💓`")
+    sleep(1)
+    await typew.edit("`Tapi Bo'ong`")
+
+
+# Create by myself @localheart
+
+
+@rewe_cmd(pattern="helikopter(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "▬▬▬.◙.▬▬▬ \n"
+        "═▂▄▄▓▄▄▂ \n"
+        "◢◤ █▀▀████▄▄▄▄◢◤ \n"
+        "█▄ █ █▄ ███▀▀▀▀▀▀▀╬ \n"
+        "◥█████◤ \n"
+        "══╩══╩══ \n"
+        "╬═╬ \n"
+        "╬═╬ \n"
+        "╬═╬ \n"
+        "╬═╬ \n"
+        "╬═╬ \n"
+        "╬═╬ \n"
+        "╬═╬ Hallo Semuanya :) \n"
+        "╬═╬☻/ \n"
+        "╬═╬/▌ \n"
+        "╬═╬/ \\ \n",
+    )
+
+
+@rewe_cmd(pattern="tembak(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "_/﹋\\_\n" "(҂`_´)\n" "<,︻╦╤─ ҉\n" r"_/﹋\_" "\n**Mau Jadi Pacarku Gak?!**",
+    )
+
+
+@rewe_cmd(pattern="bundir(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "`Dadah Semuanya...`          \n　　　　　|"
+        "\n　　　　　| \n"
+        "　　　　　| \n"
+        "　　　　　| \n"
+        "　　　　　| \n"
+        "　　　　　| \n"
+        "　　　　　| \n"
+        "　　　　　| \n"
+        "　／￣￣＼| \n"
+        "＜ ´･ 　　 |＼ \n"
+        "　|　３　 | 丶＼ \n"
+        "＜ 、･　　|　　＼ \n"
+        "　＼＿＿／∪ _ ∪) \n"
+        "　　　　　 Ｕ Ｕ\n",
+    )
+
+
+@rewe_cmd(pattern="awk(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "────██──────▀▀▀██\n"
+        "──▄▀█▄▄▄─────▄▀█▄▄▄\n"
+        "▄▀──█▄▄──────█─█▄▄\n"
+        "─▄▄▄▀──▀▄───▄▄▄▀──▀▄\n"
+        "─▀───────▀▀─▀───────▀▀\n`Awkwokwokwok..`",
+    )
+
+
+@rewe_cmd(pattern="ular(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "░░░░▓\n"
+        "░░░▓▓\n"
+        "░░█▓▓█\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░░░██▓▓██\n"
+        "░░░░██▓▓██\n"
+        "░░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░██▓▓██\n"
+        "░░░██▓▓███\n"
+        "░░░░██▓▓████\n"
+        "░░░░░██▓▓█████\n"
+        "░░░░░░██▓▓██████\n"
+        "░░░░░░███▓▓███████\n"
+        "░░░░░████▓▓████████\n"
+        "░░░░█████▓▓█████████\n"
+        "░░░█████░░░█████●███\n"
+        "░░████░░░░░░░███████\n"
+        "░░███░░░░░░░░░██████\n"
+        "░░██░░░░░░░░░░░████\n"
+        "░░░░░░░░░░░░░░░░███\n"
+        "░░░░░░░░░░░░░░░░░░░\n",
+    )
+
+
+@rewe_cmd(pattern="y(?: |$)(.*)")
+async def _(event):
+    await edit_or_reply(
+        event,
+        "‡‡‡‡‡‡‡‡‡‡‡‡▄▄▄▄\n"
+        "‡‡‡‡‡‡‡‡‡‡‡█‡‡‡‡█\n"
+        "‡‡‡‡‡‡‡‡‡‡‡█‡‡‡‡█\n"
+        "‡‡‡‡‡‡‡‡‡‡█‡‡‡‡‡█\n"
+        "‡‡‡‡‡‡‡‡‡█‡‡‡‡‡‡█\n"
+        "██████▄▄█‡‡‡‡‡‡████████▄\n"
+        "▓▓
